e18087ea579bf9fa948b717f86e816cebe6cc6db
Thomas Delmotte <marcodlmt@gmail.com>
2026-01-05 15:39:44 +0100

    feat add sanitizer

--- HelpDesk/src/app.js
+++ HelpDesk/src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { ApiError } = require('./errors/apiError');
 const {globalLimiter} = require('./middlewares/rateLimiter');
+const sanitizer = require('./middlewares/sanitizer');
 
 // pour gerer les parms URL ( éviter les double id par exemple)
 const hpp = require('hpp');
@@ -16,7 +17,7 @@ require('./config/passport');
 
 // Initialisation de Cors
 // Liste des domaines autorisés
-const whitelist = ['http://localhost:5500', 'http://localhost:4200'];
+const whitelist = ['http://localhost:5500', 'http://localhost:4200', 'http://localhost:3000'];
 
 
 // Origin c'est ce qu'envoie un serv a un navigateur
@@ -46,19 +47,27 @@ const logger = require('./middlewares/logger.middleware');
 const errorHandler = require('./errors/errorHandler');
 
 const app = express();
-
+app.use(express.json());
+app.use(sanitizer);
+// Stockage temporaire en mémoire (pour la démo)
+const messages = [];
+app.get('/messages', (req, res) => res.json(messages));
+app.post('/messages', (req, res) => { 
+// Faille : On stocke directement ce qu'on reçoit sans nettoyer
+const { content } = req.body; messages.push({ content, date: new Date() });
+res.json({ status: 'success' }); });
 
 app.use(logger);
 
 // --- Middleware de sécurité ---
-app.use(helmet());
+//app.use(helmet());
 // Utiliser les option Cors pour proteger le HEADER des attaques extérieur
-app.use(cors(corsOption));
-app.use(globalLimiter);
+//app.use(cors(corsOption));
+//app.use(globalLimiter);
 
 // Parser JSON
-app.use(express.json());
-app.use(passport.initialize());
+
+//app.use(passport.initialize());
 
 
 // A placer  avant les routes mais après le body parser ! pour nettoyer toutes les requetes entrantes
@@ -66,12 +75,16 @@ app.use(hpp())
 
 
 
-
+// Déservir les fichiers statiques
+app.use(express.static('public'));
 
 // --- Définition des Routes ---
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
+
+// Routes de TEST
+app.get('/test-hpp', (req, res) => { console.log("Paramètre id reçu :", req.query.id); res.send(req.query.id); });
 
 app.use((req, res, next) => {
     next(new ApiError(404, 'Not Found'));
